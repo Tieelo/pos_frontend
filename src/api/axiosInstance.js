fd264e52f0ec7c9654e6903704bc10175c59fe0e
Mirco Franzkowiak <mirco.franzkowiak@zurich.com>
2023-10-19 13:30:57 +0200

    Initial commit

---
+++ src/api/axiosInstance.js
@@ -0,0 +1,9 @@
+// axiosInstance.js
+
+import axios from 'axios'
+
+const axiosInstance = axios.create({
+    baseURL: 'http://localhost:8080'
+});
+
+export default axiosInstance;
---
+++ src/components/Groups.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <table>
+      <thead>
+      <tr>
+        <th>ID</th>
+        <th>Name</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="group in groups" :key="group.id">
+        <td>{{ group.id }}</td>
+        <td>{{ group.name }}</td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import { ref, onMounted } from 'vue'
+import axiosInstance from '@/api/axiosInstance' // Pfad anpassen, falls notwendig
+
+export default {
+  setup() {
+    const groups = ref([])
+
+    onMounted(async () => {
+      const response = await axiosInstance.get('/api/groups')
+      groups.value = response.data
+    })
+
+    return {
+      groups
+    }
+  }
+}
+</script>
