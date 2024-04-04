edc4451bc48ea5e3b4c8cede3995aa82286aaac9
Mirco Franzkowiak <mirco.franzkowiak@zurich.com>
2024-04-04 21:54:11 +0200

    change base path in vite.config.js for building files

--- Dockerfile
+++ Dockerfile
@@ -5,4 +5,4 @@ USER root
 RUN apk upgrade --no-cache &&\
     apk add --no-cache bash curl
 
-COPY /dist/ /usr/share/nginx/html/
+COPY /dist/ /usr/share/nginx/html/pos/
--- scripts/kill.sh
+++ scripts/kill.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 set -o errexit
-cd "$(dirname "$(dirname "$0")")"
+cd "$(dirname "$0")" && cd ..
+
+pwd
+#rm -rf node_modules/
+#rm -rf dist/
 
 export APPLICATION="pos_frontend"
 export VERSION=$(grep -m 1 "^#" semver.txt | cut -c 2-)
 export TAG="${APPLICATION}:${VERSION}"
 
-rm -rf node_modules/
-rm -rf dist/
 docker kill ${APPLICATION}
 docker rm ${APPLICATION}
 docker rmi ${TAG}
