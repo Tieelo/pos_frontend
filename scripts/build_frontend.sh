900b216010106661f84699a9def7fbf3ec5d90cd
Mirco Franzkowiak <mirco.franzkowiak@zurich.com>
2024-04-12 23:30:52 +0200

    Add new Traefik rules and improve build scripts
    Added Middlewares, Chains & HTTP/TCP rules on Traefik. Also, modified the Dockerfile instructions and scripts used to build the frontend/backend application. Improved the gitignore file to match the project requirements.

--- scripts/build_frontend.sh
+++ scripts/build_frontend.sh
@@ -3,26 +3,26 @@ set -o errexit
 
 source .secrets # enthält die Variablen DOCKER_USERNAME, DOCKER_PASSWORD und GITHUB_TOKEN
 export BRANCH="feature/containerization"
-export APPLICATION="pos_fronted"
-export USER="Tieelo"
+export APPLICATION="pos_frontend"
+export USER="tieelo"
 
 git clone --branch=${BRANCH} https://${GITHUB_USERNAME}:${GITHUB_TOKEN}@github.com/${USER}/${APPLICATION}.git
 cd ${APPLICATION}
 
 export VERSION="$(grep -m 1 "^#" semver.txt | cut -c 2-)"
-export TAG="${APPLICATION}:${VERSION}"
-
-git clone --branch=${BRANCH} https://${USER}:${TOKEN}@github.com/${USER}/${APPLICATION}.git
-cd ${APPLICATION}
+export TAG="${USER}/${APPLICATION}:${VERSION}"
+export LATEST="${USER}/${APPLICATION}:latest"
 
 #npm install
-npm ci
+npm install
 npm run build
 
 echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin
-docker build -f Dockerfile --no-cache -t ${TAG}
-docker push ${DOCKER_USERNAME}/${APPLICATION}:latest
-docker push ${DOCKER_USERNAME}/${TAG}
+docker build -f Dockerfile --no-cache -t ${TAG} .
+docker tag ${TAG} ${LATEST}
+docker push ${LATEST}
+docker push ${TAG}
+docker logout
 
 cd ..
-rm -fr ${APPLICATION}
+rm -fr ${APPLICATION}/
