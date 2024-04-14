ac6a147be9bf9e27313df7629c2e72aa9b367232
Mirco <mirco.franzkowiak@zurich.com>
2024-04-14 21:49:11 +0200

    Update backend address replacement in script
    Updated sed command in the script '40-change-backend-address.sh' to correctly replace the backend URL. This change ensures that the new backend URL is accurately reflected in the 'index-*.js' files, improving the accuracy and reliability of the POs frontend.

--- scripts/40-change-backend-address.sh
+++ scripts/40-change-backend-address.sh
@@ -6,7 +6,7 @@ if [ -n "${BACKEND_URL}" ]; then
     FILE_PATH=$(ls /usr/share/nginx/html/assets/index-*.js)
 
     # Suchen und Ersetzen der URL in der Datei
-    sed -i "s|{baseURL:\"http://pos_backend:8080\"}|{baseURL:\"${BACKEND_URL}\"}|" "${FILE_PATH}"
-    sed -i "s|http://pos_backend:8080|${BACKEND_URL}|" "${FILE_PATH}"
-    sed -i "s|{baseURL:\"http://pos_backend:8080\",withCredentials:!0}|{baseURL:\"${BACKEND_URL}\",withCredentials:!0}|" "${FILE_PATH}"
+    # sed -i "s|{baseURL:\"http://pos_backend:8080\",withCredentials:!0}|{baseURL:\"${BACKEND_URL}\",withCredentials:!0}|" "${FILE_PATH}"
+    # sed -i "s|{baseURL:\"http://pos_backend:8080\"}|{baseURL:\"${BACKEND_URL}\"}|" "${FILE_PATH}"
+    sed -r "s|{baseURL:\"http://pos_backend:8080\"|{baseURL:\"${BACKEND_URL}\"|" "${FILE_PATH}"
 fi
