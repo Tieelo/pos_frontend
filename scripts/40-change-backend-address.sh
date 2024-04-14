5bf979b034613e39bbd20d97cda60f848311c46e
Mirco <mirco.franzkowiak@zurich.com>
2024-04-14 17:42:54 +0200

    Add withCredentials attribute to axiosInstance
    This commit adds a new attribute, withCredentials, to the axiosInstance which is set to true. The withCredentials setting is used to include the cookie or authorization data in requests to the server.

--- scripts/40-change-backend-address.sh
+++ scripts/40-change-backend-address.sh
@@ -7,4 +7,6 @@ if [ -n "${BACKEND_URL}" ]; then
 
     # Suchen und Ersetzen der URL in der Datei
     sed -i "s|{baseURL:\"http://pos_backend:8080\"}|{baseURL:\"${BACKEND_URL}\"}|" "${FILE_PATH}"
+    sed -i "s|http://pos_backend:8080|${BACKEND_URL}|" "${FILE_PATH}"
+    sed -i "s|{baseURL:\"http://pos_backend:8080\",withCredentials:!0}|{baseURL:\"${BACKEND_URL}\",withCredentials:!0}|" "${FILE_PATH}"
 fi
