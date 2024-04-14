fc0637c5189616f2a47398d8ae51cb9653449858
Mirco <mirco.franzkowiak@zurich.com>
2024-04-14 21:53:31 +0200

    Update sed command in backend address change script
    The sed command used to change the backend address in the pos_frontend script has been updated. The "-i" option was added to perform the replacement directly in the original file instead of only outputting the modified content. This change ensures the actual file contents get updated with the new backend URL.

--- scripts/40-change-backend-address.sh
+++ scripts/40-change-backend-address.sh
@@ -8,5 +8,5 @@ if [ -n "${BACKEND_URL}" ]; then
     # Suchen und Ersetzen der URL in der Datei
     # sed -i "s|{baseURL:\"http://pos_backend:8080\",withCredentials:!0}|{baseURL:\"${BACKEND_URL}\",withCredentials:!0}|" "${FILE_PATH}"
     # sed -i "s|{baseURL:\"http://pos_backend:8080\"}|{baseURL:\"${BACKEND_URL}\"}|" "${FILE_PATH}"
-    sed -r "s|{baseURL:\"http://pos_backend:8080\"|{baseURL:\"${BACKEND_URL}\"|" "${FILE_PATH}"
+    sed -i "s|{baseURL:\"http://pos_backend:8080\"|{baseURL:\"${BACKEND_URL}\"|" "${FILE_PATH}"
 fi
