ca1aa1b7cddcfe7d47467404ce05ef39826f63a0
Mirco <mirco.franzkowiak@zurich.com>
2024-04-22 23:27:24 +0200

    Refactor cart data and button actions in frontend
    The cart information (items, total price, item count) has been refactored to be managed in App.vue instead of within the CartDisplay.vue component itself. This has simplified CartDisplay.vue by making it a pure presentation component. The handling of button actions in StaticButtonRow.vue has been adjusted as well, with the addition of emit calls to update the cart data after a sell or return action.

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -4,6 +4,7 @@ import axios from 'axios'
 
 const axiosInstance = axios.create({
     baseURL: 'http://pos_backend:8080',
+/*    baseURL: 'http://192.168.10.167:8080',*/
     withCredentials: true
     /*baseURL: import.meta.env.VITE_BACKEND_URL || 'http://localhost:11386'*/
 })
